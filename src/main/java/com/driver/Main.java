0f72d51a0e991fa114c51e37795bd55dc972132b
Panthulumahitha <panthulumahitha0620@gmail.com>
2024-11-29 14:13:31 +0530

    Main.java

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,15 +1,9 @@
 package com.driver;
 
 public class Main {
-   public static void main(String[] args) {
-        // Task 3: Create an object of class B and call meth from class A
+    public static void main(String[] args) {
         B obj = new B();
-
-        // Before overriding
         System.out.println("Output from class A's method: " + obj.meth());
-
-        // After overriding
         System.out.println("Output from overridden method in class B: " + obj.meth());
     }
-
 }
