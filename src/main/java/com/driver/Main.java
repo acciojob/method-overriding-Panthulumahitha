7590a977d00486fc90267e7c7b53699a97f893d1
Panthulumahitha <panthulumahitha0620@gmail.com>
2024-11-29 14:49:49 +0530

    Main.java

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,9 +1,11 @@
 package com.driver;
 
+
+
+// Main.java
 public class Main {
     public static void main(String[] args) {
         B obj = new B();
-        System.out.println("Output from class A's method: " + obj.meth());
-        System.out.println("Output from overridden method in class B: " + obj.meth());
+        System.out.println(obj.meth()); // Calls the overridden method in class B
     }
 }
