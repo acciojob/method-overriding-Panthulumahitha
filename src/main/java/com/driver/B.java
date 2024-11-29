e37bdce71983337fe8a0ddd949aba86287d9e06c
Panthulumahitha <panthulumahitha0620@gmail.com>
2024-11-29 14:00:39 +0530

    B.java

---
+++ src/main/java/com/driver/B.java
@@ -0,0 +1,9 @@
+package com.driver;
+
+    public class B extends A {
+    // Task 4: Override meth in class B
+    @Override
+    public String meth() {
+        return "Method is overridden in Extended class B";
+    }
+}
