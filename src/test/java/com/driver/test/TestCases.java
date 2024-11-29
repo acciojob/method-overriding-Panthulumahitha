725cd7faa92f51a7854e289c43606251349dc7c7
Panthulumahitha <panthulumahitha0620@gmail.com>
2024-11-29 14:17:44 +0530

    TestCases.java

--- src/test/java/com/driver/test/TestCases.java
+++ src/test/java/com/driver/test/TestCases.java
@@ -1,8 +1,14 @@
 package com.driver.test;
 
-import static org.junit.Assert.*;
-import org.junit.jupiter.api.Test;
+import com.driver.A;
+import com.driver.B;
 
 public class TestCases {
-    
+    public static void main(String[] args) {
+        A objA = new A();
+        System.out.println(objA.meth());
+
+        B objB = new B();
+        System.out.println(objB.meth());
+    }
 }
