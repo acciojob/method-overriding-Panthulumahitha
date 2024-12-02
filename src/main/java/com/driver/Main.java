a380f5213fcc4d599076ce3e7bd4a5be17f0b4d7
Panthulumahitha <panthulumahitha0620@gmail.com>
2024-12-02 20:24:19 +0530

    Main.java

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -2,15 +2,13 @@ package com.driver;
 
 // Main.java
 public class Main {
-    // Class A defined inside Main
-    static class A {
+    public static class A {
         public String meth() {
             return "Invoking method from class A";
         }
     }
 
-    // Class B defined inside Main
-    static class B extends A {
+    public static class B extends A {
         @Override
         public String meth() {
             return "Method is overridden in Extended class B";
