e270e97ca830071da3fa7fd96b4033928c7e0724
Panthulumahitha <panthulumahitha0620@gmail.com>
2024-12-02 20:20:55 +0530

    TestCases.java

--- src/test/java/com/driver/test/TestCases.java
+++ src/test/java/com/driver/test/TestCases.java
@@ -1,14 +1,20 @@
 package com.driver.test;
 
-import com.driver.A;
-import com.driver.B;
+import org.junit.Test;
+import static org.junit.Assert.*;
 
 public class TestCases {
-    public static void main(String[] args) {
-        A objA = new A();
-        System.out.println(objA.meth());
+    @Test
+    public void testMethInClassA() {
+        Main.A a = new Main.A();
+        String result = a.meth();
+        assertEquals("Invoking method from class A", result);
+    }
 
-        B objB = new B();
-        System.out.println(objB.meth());
+    @Test
+    public void testMethInClassB() {
+        Main.B b = new Main.B();
+        String result = b.meth();
+        assertEquals("Method is overridden in Extended class B", result);
     }
 }
